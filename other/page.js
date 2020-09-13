1aec44cc5186970c205fb35629bf1cbac3c86bf9
Daniel <65277548+Daniel4-Scratch@users.noreply.github.com>
2020-09-13 11:18:37 +1000

    Update page.js

--- other/page.js
+++ other/page.js
@@ -1,7 +1,7 @@
 var shut = false;
 
 var template = function(name, body) {
-  document.body.innerHTML = '<head><title>'+ name +' - Cope</title><link rel="stylesheet" href="other/page.css"></head><body><div class="dash">&nbsp; <b>Cope</b>&nbsp;&nbsp;&nbsp; <a href="https://dpla-s.github.io/">Home</a> <a href="https://dpla-s.github.io/Cope-DPLA">About</a> <span class="dash">&nbsp;</span> <a class="white" href="https://dpla-s.github.io/Cope-DPLA/#nice-were-can-i-downloaduse-this">Download</a> <div class="f"> <a class="white" style="float:right;" href="https://github.com/DPLA-S" target="_blank">Github</a> <span style="float:right;">&nbsp</span> </div> </div><div class="te">'+
+  document.body.innerHTML = '<head><title>'+ name +' - Cope</title><link rel="stylesheet" href="other/page.css"></head><body><div class="dash">&nbsp; <b>Cope</b>&nbsp;&nbsp;&nbsp; <a href="https://dpla-s.github.io/">Home</a><a href="https://dpla-s.github.io/Cope-DPLA">About</a><a href="https://dpla-s.github.io/document">Documentation</a> <span class="dash">&nbsp;</span> <a class="white" href="https://dpla-s.github.io/Cope-DPLA/#nice-were-can-i-downloaduse-this">Download</a> <div class="f"> <a class="white" style="float:right;" href="https://github.com/DPLA-S" target="_blank">Github</a> <span style="float:right;">&nbsp</span> </div> </div><div class="te">'+
   body + '</div></body>';
   
   if (shut) {
